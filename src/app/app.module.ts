feb89466d0ea95bdde310d904c8a6bf7f744a9e4
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-08-28 19:00:27 +0100

    initial commit - inc json-server components services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
